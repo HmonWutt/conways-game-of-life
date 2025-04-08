78ad6304fab3fa09f75b8c70ff2b1c64ffa06039
hmon <wtthumon@gmail.com>
2025-04-08 07:46:52 +0200

    move board set up inside function of run button

--- index.js
+++ index.js
@@ -13,37 +13,17 @@ const simulationWindow = document.querySelector(".simulationWindow");
 
 let isRunning = false;
 let intervalId;
-let rows=10;
-let columns=10;
+/* let rows=10;
+let columns=10; */
 
 
 let grid = [];
 let savedData;
-rowButton.addEventListener("click", () => {rows = parseInt(rowInput.value); console.log(rows)});
-colButton.addEventListener(
-  "click",
-  () => {columns = parseInt(colInput.value); console.log(columns)}
-);
-let count = 0;
-for (let row = 0; row < rows; row++) {
-
-    for (let column = 0; column < columns; column++) {
-        grid.push({
-            id: count,
-            row: row,
-            column: column,
-            alive: false,
-        })
-        count++
-    }
-}
-
-canvas.style.gridTemplateColumns = `repeat(${columns}, var(--size))`
-renderCells()
+/* rowButton.addEventListener("click", () => rows = parseInt(rowInput.value));
+colButton.addEventListener("click", () => columns = parseInt(colInput.value)); */
 
 function renderCells() {
   canvas.innerHTML = "";
-
   grid.forEach((cell) => {
     const cellNode = document.createElement("div");
 
@@ -65,9 +45,22 @@ runButton.addEventListener('pointerdown', () => {
     isRunning = true;
     canvas.classList.add('isRunning')
     savedData = JSON.stringify(grid)
-    
-
-
+    let count = 0;
+    rows = rowInput.value
+    columns = colInput.value
+    for (let row = 0; row < rows; row++) {
+      for (let column = 0; column < columns; column++) {
+        grid.push({
+          id: count,
+          row: row,
+          column: column,
+          alive: false,
+        });
+        count++;
+      }
+    }
+    canvas.style.gridTemplateColumns = `repeat(${columns}, var(--size))`;
+    renderCells();
     const simulateGeneration = () => {
         const url = "http://127.0.0.1:5000/getAliveCells";
         fetch(url)
@@ -88,7 +81,6 @@ runButton.addEventListener('pointerdown', () => {
           });
     }
     simulateGeneration()
-
     const time = parseInt(duration.value) || 1500
     intervalId = setInterval(simulateGeneration, time)
 })
