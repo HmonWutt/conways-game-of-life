4527e6a3c323495dd34615fa04a4dd6511284e53
hmon <wtthumon@gmail.com>
2025-04-08 10:54:24 +0200

    added post method, added hide show for the buttons'

--- backend.py
+++ backend.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template_string
+from flask import Flask,render_template_string,request
 from driver import driver
 from flask_cors import CORS
 import json
@@ -28,26 +28,28 @@ def get_alive_cells():
     )
     return records[0].data()['alive']
 
-@app.route("/createNodesAndRelationships")
+@app.route("/createNodesAndRelationships",methods=['POST'])
 def create_nodes_and_relationships():
-    grid = request.args.get(grid)
-    for each in grid:
-        summary = driver.execute_query(
-            """
-            CREATE (c:Cell {id: $id, alive: $alive, neighbours: $neighbours})
-            WITH c
-            UNWIND split(substring(c.neighbours,1,size(c.neighbours)-2),",") as n
-            CREATE (c)-[:NEIGHBOUR]->(c1{id: toInteger(n)})
-            WITH c
-            REMOVE c.neighbours
-            """,
-            id = each.id,
-            alive = each.alive,
-            neighbours = each.neighbours,
-            database_="neo4j",
-        ).summary
-        print("Created {nodes_created} nodes in {time} ms.".format(
-            nodes_created=summary.counters.nodes_created,
-            time=summary.result_available_after
-        ))
-    
+    if request.method == 'POST':
+        data = request.get_json()
+        grid = data.get('grid')
+        for each in grid:
+            summary = driver.execute_query(
+                """
+                CREATE (c:Cell {id: $id, alive: $alive, neighbours: $neighbours})
+                WITH c
+                UNWIND split(substring(c.neighbours,1,size(c.neighbours)-2),",") as n
+                CREATE (c)-[:NEIGHBOUR]->(c1{id: toInteger(n)})
+                WITH c
+                REMOVE c.neighbours
+                """,
+                id = each.id,
+                alive = each.alive,
+                neighbours = each.neighbours,
+                database_="neo4j",
+            ).summary
+            print("Created {nodes_created} nodes in {time} ms.".format(
+                nodes_created=summary.counters.nodes_created,
+                time=summary.result_available_after
+            ))
+        
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 const canvas = document.querySelector(".canvas");
+const readyButton = document.querySelector("#ready");
 const runButton = document.querySelector("#run");
 const stopButton = document.querySelector("#stop");
 const resetButton = document.querySelector("#reset");
@@ -8,59 +9,58 @@ const rowInput = document.querySelector("#rows");
 const colInput = document.querySelector("#cols");
 const rowButton = document.querySelector("#rowButton");
 const colButton = document.querySelector("#colButton");
-const durationButton = document.querySelector("#duration");
+const durationButton = document.querySelector("#durationButton");
 const simulationWindow = document.querySelector(".simulationWindow");
 
 let isRunning = false;
 let intervalId;
 let grid = [];
 let savedData;
+colButton.addEventListener('click',()=>{
+      let count = 0;
+      rows = rowInput.value;
+      columns = colInput.value;
+      for (let row = 0; row < rows; row++) {
+        for (let column = 0; column < columns; column++) {
+          grid.push({
+            id: count,
+            row: row,
+            column: column,
+            alive: false,
+            neighbours: [],
+          });
+          count++;
+        }
+      }
+      canvas.style.gridTemplateColumns = `repeat(${columns}, var(--size))`;
+      renderCells();
+      readyButton.classList.remove("hide");
+      rowInput.classList.add("hide");
+      colInput.classList.add("hide");
+      duration.classList.add("hide");
+      rowButton.classList.add("hide");
+      colButton.classList.add("hide");
+      durationButton.classList.add("hide");
+      rowButton.classList.add("hide");
+      colButton.classList.add("hide");
+})
+readyButton.addEventListener('pointerdown', () => {
+    makeNeighbours(grid)
+    console.log("initial state",grid.forEach((cell)=>console.log(cell.alive,cell.neighbours)))
+    //createNodesAndRelationships();
+    readyButton.classList.add("hide");
+    runButton.classList.remove("hide");
+  })
 
 runButton.addEventListener('pointerdown', () => {
-    isRunning = true;
-    canvas.classList.add('isRunning')
-    savedData = JSON.stringify(grid)
-    let count = 0;
-    rows = rowInput.value
-    columns = colInput.value
-    for (let row = 0; row < rows; row++) {
-      for (let column = 0; column < columns; column++) {
-        grid.push({
-          id: count,
-          row: row,
-          column: column,
-          alive: false,
-          neighbours:[]
-        });
-        count++;
-      }
-    }
-    canvas.style.gridTemplateColumns = `repeat(${columns}, var(--size))`;
-    renderCells();
-    makeNeighbours(grid)
-    console.log("initial state",grid.forEach((cell)=>console.log(cell.neighbours)))
-    const simulateGeneration = () => {
-        const url = "http://127.0.0.1:5000/getAliveCells";
-        fetch(url)
-          .then((response) => {
-            if (!response.ok) {
-              throw new Error(`HTTP error ${response.status}`);
-            }
-            return response.text();
-          })
-          .then((data) => {
-            console.log("data", data);
-            //bucket.textContent = data;
-            loopThroughCellsAndTurnOnOrOff(data)
-            
-          })
-          .catch((error) => {
-            // ...handle/report error...
-          });
-    }
+   isRunning = true;
+   canvas.classList.add("isRunning");
+   savedData = JSON.stringify(grid);
     simulateGeneration()
     const time = parseInt(duration.value) || 1500
     intervalId = setInterval(simulateGeneration, time)
+    stopButton.classList.remove("hide");
+    resetButton.classList.remove("hide");
 })
 
 stopButton.addEventListener('pointerdown', () => {
@@ -75,13 +75,24 @@ stopButton.addEventListener('pointerdown', () => {
 resetButton.addEventListener('pointerdown', () => {
     grid.map(o => o.alive = false);
     renderCells()
+    rowInput.classList.remove("hide");
+    colInput.classList.remove("hide")
+    duration.classList.remove("hide");
+    rowButton.classList.remove("hide");
+    colButton.classList.remove("hide");
+    durationButton.classList.remove("hide");
+    rowButton.classList.remove("hide");
+    colButton.classList.remove("hide");
+    readyButton.classList.add("hide")
+    runButton.classList.add("hide")
+    stopButton.classList.add("hide");
+    resetButton.classList.add("hide");
 })
 
 function renderCells() {
   canvas.innerHTML = "";
   grid.forEach((cell) => {
     const cellNode = document.createElement("div");
-
     cellNode.classList.add("cell");
     cellNode.id = cell.id;
     cellNode.classList.toggle("alive", cell.alive);
@@ -92,10 +103,22 @@ function renderCells() {
     cellNode.addEventListener("pointerdown", () => {
       cell.alive = !cell.alive;
       renderCells();
-    });
+    }); 
   });
 }
 
+function pickAliveCells(){
+  if (isRunning) return;
+  grid.forEach((cell)=>{
+    const cellNode = document.querySelector("#cell.id")
+    cellNode.addEventListener("pointerdown", () => {
+    cell.alive = !cell.alive;
+    cellNode.classList.toggle("alive", cell.alive);
+    
+  });
+})
+renderCells();
+}
 function loopThroughCellsAndTurnOnOrOff(aliveCells){
   const cells = document.querySelectorAll(".cell");
    cells.forEach((cell) => {
@@ -120,4 +143,37 @@ function makeNeighbours(grid){
     }
   }
   
-    
+function simulateGeneration(){
+        const url = "http://127.0.0.1:5000/getAliveCells";
+        fetch(url)
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error(`HTTP error ${response.status}`);
+            }
+            return response.text();
+          })
+          .then((data) => {
+            console.log("data", data);
+            //bucket.textContent = data;
+            loopThroughCellsAndTurnOnOrOff(data)
+            
+          })
+          .catch((error) => {
+            // ...handle/report error...
+          });
+    }
+
+function createNodesAndRelationships(){
+  fetch("http://127.0.0.1:5000/createNodesAndRelationships", {
+    method: "POST",
+    body: JSON.stringify({
+      "grid": grid
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+
+}
